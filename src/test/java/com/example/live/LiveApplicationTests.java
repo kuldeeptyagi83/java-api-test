517f57b5b1678c7c9f415a2ce956572650a93840
KTyagiLinz <KTyagi@linz.govt.nz>
2025-02-16 06:01:08 +1300

    added karate test with gradle

--- src/test/java/com/example/live/LiveApplicationTests.java
+++ src/test/java/com/example/live/LiveApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.live;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")
 class LiveApplicationTests {
-
 	@Test
 	void contextLoads() {
+		// This method will pass if the application context loads successfully
 	}
 
 }
